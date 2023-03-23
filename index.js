ac2b77db46812542371d35aaf7cfbdeb42b95eac
Hande Demirbay <handedemirbay@gmail.com>
2023-03-23 18:41:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+  const messages = [];
+
+  for(let i = 0; i < names.length; i++){
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+  while(number >= 0){
+      console.log(number--);
+  }
+}
+countDown(10);
